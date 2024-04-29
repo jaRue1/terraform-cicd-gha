852bcac999dd54a00538aa99d0dd008e71260845
jaRue1 <jaruejohnson@outlook.com>
2024-04-28 23:03:09 -0400

    test staging removal

--- env/staging/main.tf
+++ env/staging/main.tf
@@ -9,16 +9,16 @@ terraform {
 }
 
 
-module "staging-lambda" {
-  source        = "../../core/hw-lambda"
-  handler       = "lambda.handler"
-  runtime       = "nodejs16.x"
-  environment   = "staging"
-  function_name = "staging-lambda"
-  filename      = "../../core/hw-lambda/function/lambda.zip"
-  source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
-  environment_variables = {
-   LAMBDA_ENVIRONMENT = "staging"
-   LAMBDA_MESSAGE = "Hello World from Staging!"
-  }
-}
+# module "staging-lambda" {
+#   source        = "../../core/hw-lambda"
+#   handler       = "lambda.handler"
+#   runtime       = "nodejs16.x"
+#   environment   = "staging"
+#   function_name = "staging-lambda"
+#   filename      = "../../core/hw-lambda/function/lambda.zip"
+#   source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
+#   environment_variables = {
+#    LAMBDA_ENVIRONMENT = "staging"
+#    LAMBDA_MESSAGE = "Hello World from Staging!"
+#   }
+# }
