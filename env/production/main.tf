1c955be6bc5730500fcfd3b0b73d9d99ee01a76d
jaRue1 <jaruejohnson@outlook.com>
2024-04-28 22:07:41 -0400

    updated values

--- env/production/main.tf
+++ env/production/main.tf
@@ -1,13 +1,13 @@
-module "staging-lambda" {
+module "production-lambda" {
   source        = "../../core/hw-lambda"
   handler       = "lambda.handler"
   runtime       = "nodejs16.x"
-  environment   = "staging"
-  function_name = "staging-lambda"
+  environment   = "production"
+  function_name = "production-lambda"
   filename      = "../../core/hw-lambda/function/lambda.zip"
   source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
   environment_variables = {
-   LAMBDA_ENVIRONMENT = "staging"
-   LAMBDA_MESSAGE = "Hello World from Staging!"
+   LAMBDA_ENVIRONMENT = "production"
+   LAMBDA_MESSAGE = "Hello World from Production!"
   }
 }
