294f1d17713672122ffafcdaf886d6e8717c221a
jaRue1 <jaruejohnson@outlook.com>
2024-04-28 22:47:42 -0400

    test

--- env/production/main.tf
+++ env/production/main.tf
@@ -9,16 +9,16 @@ terraform {
 }
 
 
-module "production-lambda" {
-  source        = "../../core/hw-lambda"
-  handler       = "lambda.handler"
-  runtime       = "nodejs16.x"
-  environment   = "production"
-  function_name = "production-lambda"
-  filename      = "../../core/hw-lambda/function/lambda.zip"
-  source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
-  environment_variables = {
-   LAMBDA_ENVIRONMENT = "production"
-   LAMBDA_MESSAGE = "Hello World from Production!"
-  }
-}
+# module "production-lambda" {
+#   source        = "../../core/hw-lambda"
+#   handler       = "lambda.handler"
+#   runtime       = "nodejs16.x"
+#   environment   = "production"
+#   function_name = "production-lambda"
+#   filename      = "../../core/hw-lambda/function/lambda.zip"
+#   source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
+#   environment_variables = {
+#    LAMBDA_ENVIRONMENT = "production"
+#    LAMBDA_MESSAGE = "Hello World from Production!"
+#   }
+# }
