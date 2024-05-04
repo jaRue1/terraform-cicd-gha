61bbac51f517392b06c5a69dd4bbcc464d12ebb3
jaRue1 <jaruejohnson@outlook.com>
2024-05-04 09:48:05 -0400

    live test

--- env/production/main.tf
+++ env/production/main.tf
@@ -5,12 +5,32 @@ terraform {
       version = "~> 5.47.0"
     }
   }
-  # backend "s3" {
-  #   bucket         = "your-bucket-name"
-  #   key            = "production/terraform.tfstate"
-  #   region         = "your-region"
-  #   dynamodb_table = "your-table-name"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "terraform-state-bucket-gha-2024"
+    key            = "production/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-table-gha-2024"
+    encrypt        = true
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-prod-test-bucket-gha-2024"
+}
+# module "production-lambda" {
+#   source        = "../../core/hw-lambda"
+#   handler       = "lambda.handler"
+#   runtime       = "nodejs16.x"
+#   environment   = "production"
+#   function_name = "production-lambda"
+#   filename      = "../../core/hw-lambda/function/lambda.zip"
+#   source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
+#   environment_variables = {
+#    LAMBDA_ENVIRONMENT = "production"
+#    LAMBDA_MESSAGE = "Hello World from Production!"
+#   }
+# }
