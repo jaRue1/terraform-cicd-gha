3c050a544cd811fb0563df7082ecc4dc87385c5e
jaRue1 <jaruejohnson@outlook.com>
2024-05-04 10:40:09 -0400

    test removal in prod

--- env/production/main.tf
+++ env/production/main.tf
@@ -18,19 +18,19 @@ provider "aws" {
   
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-prod-test-bucket-gha-2024"
-}
-module "production-lambda" {
-  source        = "../../core/hw-lambda"
-  handler       = "lambda.handler"
-  runtime       = "nodejs16.x"
-  environment   = "production"
-  function_name = "production-lambda"
-  filename      = "../../core/hw-lambda/function/lambda.zip"
-  source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
-  environment_variables = {
-   LAMBDA_ENVIRONMENT = "production"
-   LAMBDA_MESSAGE = "Hello World from Production!"
-  }
-}
+# resource "aws_s3_bucket" "example" {
+#   bucket = "my-prod-test-bucket-gha-2024"
+# }
+# module "production-lambda" {
+#   source        = "../../core/hw-lambda"
+#   handler       = "lambda.handler"
+#   runtime       = "nodejs16.x"
+#   environment   = "production"
+#   function_name = "production-lambda"
+#   filename      = "../../core/hw-lambda/function/lambda.zip"
+#   source_code_hash = filebase64sha256("../../core/hw-lambda/function/lambda.zip")
+#   environment_variables = {
+#    LAMBDA_ENVIRONMENT = "production"
+#    LAMBDA_MESSAGE = "Hello World from Production!"
+#   }
+# }
